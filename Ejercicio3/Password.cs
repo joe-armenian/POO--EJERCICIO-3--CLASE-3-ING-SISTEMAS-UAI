98081a9ebffb383955f0f1635875ddafe89f0323
Joel Damerdjian <agustinjoel2020@gmail.com>
2024-03-08 20:45:44 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio3/Password.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Ejercicio3
+{
+    public class Password
+    {
+        private int Longitud { get; set; }
+
+        private string Contraseña { get; set; }
+
+        private static  Random rnd=new Random();
+
+        private const int longitudDefault = 8;
+        public Password()
+        {
+         this.Longitud = longitudDefault;
+         Contraseña =GenerarPassword();
+        }
+        public Password(int longitud)
+        {
+         this.Longitud = longitud;
+         Contraseña =GenerarPassword();
+        }
+        //metodo para la contraseña aleatoria
+        private string GenerarPassword()
+        {
+            StringBuilder builder = new StringBuilder(); 
+            string caracteres = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            for (int i=0;i<Longitud;i++) 
+            {
+                int indice=rnd.Next(caracteres.Length); 
+                builder.Append(caracteres[indice]);
+            }
+            return Contraseña= builder.ToString(); 
+        }
+        public bool EsFuerte()
+        {
+            int mayusculas = 0, minusculas = 0, numeros = 0;
+            foreach (char caracter in Contraseña) 
+            {
+                if (char.IsUpper(caracter))
+                {
+                    mayusculas++;
+                }
+                else if (char.IsLower(caracter)) 
+                {
+                    minusculas++;
+                }
+                else if (char.IsDigit(caracter))
+                {
+                    numeros++;
+                }
+            }
+
+            return mayusculas > 2 && minusculas > 1 && numeros > 5;
+        }
+        public string GetContraseña()
+        {
+            return Contraseña;
+        }
+        public int GetLongitud()
+        {
+            return Longitud;
+        }
+        public void SetLongitud(int longitud)
+        {
+            Longitud = longitud;
+        }
+    }
+}
---
+++ Ejercicio3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio3
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Ingrese la longitud de las contraseñas");
+            int longitud =Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ingrese la cantidad de las contraseñas");
+            int cantidad = Convert.ToInt32(Console.ReadLine());
+
+            Password[] contraseñas = new Password[cantidad];
+            bool[] esFuerte=new bool[cantidad];
+
+            
+
+            for (int i = 0;i<cantidad;i++)
+            {
+                contraseñas[i]= new Password(longitud);
+                esFuerte[i]= contraseñas[i].EsFuerte(); 
+            }
+
+            for (int i=0;i<cantidad;i++)
+            {
+                Console.WriteLine($"Contraseña {i+1}: {contraseñas[i].GetContraseña()}-¿Es segura?:{esFuerte[i]}");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
